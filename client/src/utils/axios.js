f5130e17d95abf735d38cbf8c700a71d635aa43d
zaidmohammad307-web <zaidmohammad307@gmail.com>
2025-12-30 03:36:08 +0300

    Refactor axios instance baseURL configuration
    x

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  import.meta.env.VITE_BASE_URL ||
+  import.meta.env.VITE_API_URL ||
+  'http://localhost:4000';
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL,
   withCredentials: true,
 });
 
