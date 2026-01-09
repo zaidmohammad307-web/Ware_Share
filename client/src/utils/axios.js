08a439f3d3a481891530ac23388d4fb0fc064067
zaidmohammad307-web <zaidmohammad307@gmail.com>
2026-01-09 07:10:54 +0300

    Implement Axios interceptor for token attachment
    Added an Axios interceptor to attach the token from localStorage to every request.

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -10,4 +10,26 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+// Always attach token (if exists) to every request.
+// This fixes refresh/new-tab cases where defaults are not set anymore.
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (typeof window !== 'undefined') {
+      const token = window.localStorage.getItem('token');
+
+      if (token) {
+        config.headers = config.headers || {};
+
+        // Don’t overwrite if caller already set it
+        if (!config.headers.Authorization && !config.headers.authorization) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default axiosInstance;
